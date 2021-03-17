6e4b2b89fc7904e69f5b06ed2d7655d5d8a5035f
Ekaterina Kashleva <ekashleva@axbit.ru>
2021-03-17 13:59:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const balanceEl = document.querySelector('.balance .value');
+
+const incomeTotelEl = document.querySelector('.outcome-total');
+const outcomeTotelEl = document.querySelector('.outcome-total');
+const chartEl = document.querySelector('.chart');
+
+const expenseBtn = document.querySelect('.tab1');
+const incomeBtn = document.querySelector('.tab2');
+const allBtn = document.querySelector('.tab3');
+
+const expenseList = document.querySelector('#expense .list');
+const incomeList = document.querySelector('#income .list');
+const allList = document.querySelector('#all .list');
+
+const expenceEl = document.querySelector('#expense');
+const incomeEl = document.querySelector('#income');
+const allEl = document.querySelector('#all');
+
